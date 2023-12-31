f9b2fd902b2ea3be6e3c1f5090111251b24ada4a
Eddie He <2021022249@m.scnu.edu.cn>
2023-12-31 19:37:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
     AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Eddie He', // 您的昵称 例如 tangly1024
     BIO: process.env.NEXT_PUBLIC_BIO || '有好奇心和求知欲的学生/互联网冲浪爱好者', // 作者简介
-    LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.eddiehe.xyz', // 网站地址
+    LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.eddiehe.top', // 网站地址
     KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
     // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
